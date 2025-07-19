c132f4083876beb46e011db67dc80c1527fcb894
DevFrances01 <leonardodes67@gmail.com>
2025-07-19 19:05:20 -0300

    key1.01

--- search_keyword.py
+++ search_keyword.py
@@ -1,6 +1,8 @@
 # leitor_palavra_chave.py
 
 arquivo = "Home/blog.html"
+print("BlogTotal-pagina,Home/blog.html")
+
 
 # Solicita a palavra-chave ao usuário
 palavra_chave = input("Digite a palavra-chave para buscar no arquivo: ").lower()
@@ -20,3 +22,11 @@ if encontradas:
         print(f"Linha {num_linha}: {conteudo}")
 else:
     print(f"Nenhuma linha contém a palavra '{palavra_chave}'.")
+entrada = input("Digite uma palavra-chave ou 'sair' para encerrar: ").strip()
+
+if entrada.lower() == 'sair':
+    print("Encerrando o programa.")
+    # Aqui pode usar break, exit() ou outra lógica para encerrar
+else:
+    # Continua o programa com a entrada fornecida
+    print(f"Você digitou: {entrada}")
